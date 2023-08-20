e48a5ed0d11579e4a1b42582ba7a75e55ee7028c
Mjtoffey <mjtoffey@gmail.com>
2023-08-20 17:24:22 -0400

    added style sheet

--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import Navbar from "../components/Navbar"
+import styles from "../styles/welcomepage.css"
 
 export default function Welcome() {
     return (
         <div>
             <Navbar />
-            <div className={stlye}>
+            <div className={styles.container}>
                 <p>
                     Welcome to Why Not Me: Recruiting
                 </p>
@@ -16,6 +17,8 @@ export default function Welcome() {
                     Once you have made your profile and customised your "games" your profile will be ready for recruiters to view and get into contact with you!
                 </p>
             </div>
+            <button> className={styles.sButton}</button>
+            <button> className={styles.lButton}</button>
         </div>
     );
 }
